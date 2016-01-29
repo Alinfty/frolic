2bb215638c0ba4583abdc171a0eadaa332d1e78a
Alinfty <alinfty@naver.com>
2016-01-29 08:23:50 +0000

    editing a little.

--- frolic.py
+++ frolic.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 
-import socket
-import parser
+from socket import *
+import sys
+#import parser
 
 if __name__=="__main__":
 	host=''
 	port=80
-	BUFFER_SIZE=1024
+	BUFSIZE=1024
 
-	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s=socket(AF_INET, SOCK_STREAM)
 	s.bind((host,port))
-	s.listen(1)
-	conn, addr=s.accept()
-	print ('Connected by', addr)
-	while 1:
-		data=conn.recv(BUFFER_SIZE)
-		if not data: break
-		print ("received data:", data)
-		conn.send("HTTP/2.0 200 OK\nContent-Type: text/plain\nContent-Length: 11\n\nHello world".encode('utf8'))
-		print("send complete")
-	conn.close()
+	s.listen(5)
+
+	try:
+		while 1:
+			print ("waiting...")
+			conn, addr=s.accept()
+			print ('Connected by', addr)
+	
+			while 1:
+				data=conn.recv(BUFSIZE)
+				if not data: break
+				print ("received data:", data)
+				conn.send(bytes("HTTP/2.0 200 OK\nContent-Type: text/plain\nContent-Length: 11\n\nHello world", 'utf8'))
+				print("send complete")
+			conn.close()
+
+	except KeyboardInterrupt:
+		print ("keyboard Interrupted!")
+		s.close()
